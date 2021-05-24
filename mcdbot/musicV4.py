51f30f74712e7252d9b1063dfdd60e68911f31bc
Santiago Pereira <santiago.saraiva.pereira@gmail.com>
2021-05-24 19:36:23 +0100

    music bot v4 changes

--- mcdbot/musicV4.py
+++ mcdbot/musicV4.py
@@ -1,6 +1,6 @@
 import PySimpleGUI as sg
 
-#layout==-==-==-==-==-==-==-==-
+#layout==-==-==-==-==-==-
 sg.theme('DarkAmber')
 layout = [
 
@@ -9,7 +9,7 @@ layout = [
             [sg.Button('Baixar')]
 
     ]
-#janela==-==-==-==-==-==-==-==-
+#janela==-==-==-==-==-==-==-
 janela = sg.Window('Download Music', layout)
 
 #eventos==-==-==-==-==-==-==-==-
@@ -21,24 +21,28 @@ while True:
         
         import requests
         from youtube_dl import YoutubeDL
-        
-        YDL_OPTIONS = {'format': 'bestaudio', 'noplaylist': 'True'}
+       
+        YDL_OPTIONS = { 
+            'outtmpl': 'downloaded_music/%(title)s.%(ext)s',
+            'format': 'm4a', 
+            'noplaylist': 'True'
+        }
+
         def search(arg):
             with YoutubeDL(YDL_OPTIONS) as ydl:
                 try:
                     requests.get(arg) 
                 except:
                     video = ydl.extract_info(f"ytsearch:{arg}", download=False)['entries'][0]
+                    print('====================================================\n{}\n===================================================='.format(video['title']))
                 else:
                     video = ydl.extract_info(arg, download=False)
+                
+                ydl.download([video['webpage_url']])
+            
+            return video
 
-            return video['webpage_url']
 
         valores['musica'] == str(valores['musica']).replace(' ','+')
-
-        import youtube_dl
         
-        ydl_opts = {}
-        with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-            ydl.download([valores['musica']])
-
+        search(valores['musica'])
